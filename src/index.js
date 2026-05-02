51e5adb31f9d7c78fd5a99d615ca6906416139e3
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-05-02 19:41:10 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const startBtn = document.getElementById('startBtn');
+const startMenu = document.getElementById('startMenu');
 
+startBtn.addEventListener('click', () => {
+    // Toggle visibility
+    if (startMenu.style.display === 'none') {
+        startMenu.style.display = 'block';
+    } else {
+        startMenu.style.display = 'none';
+    }
+});
