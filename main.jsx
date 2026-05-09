ef63a151e9f6e7364350d949de533d677f42b262
ErlinGuillen <guillenvelasquezerlin@gmail.com>
2026-05-08 23:09:35 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GlassBlog from './app.jsx'; // Import your blog component
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
